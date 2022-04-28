32ad82ba5a0eaa5cc63509e86d796ed64023cb36
suren-simonyan <suren.simonyan20@gmail.com>
2022-04-29 00:26:23 +0400

    some part of project spent about 2.5 - 3 hours

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header/Header';
+import Content from './components/Content/Content';
+import Footer from './components/Footer/Footer';
+
+import './assets/styles/App.scss';
+import './assets/styles/fonts.css';
+import './assets/styles/colors.css';
+import HeaderBanner from './components/HeaderBanner/HeaderBanner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="main_inner">
+        <Header />
+        <HeaderBanner />
+        <Content />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Content/Content.js
@@ -0,0 +1,96 @@
+import styles from './Content.module.scss';
+import nft from '../../assets/images/nft.jpg';
+
+const Content = () => {
+    return (
+        <section className={styles.content}>
+            <h2>
+                welcome to
+                <p>the training</p>
+            </h2>
+
+            <div className={styles.modules_wrap}>
+
+                <article className={styles.module}>
+                    <div className={styles.module_text}>
+                        <h4>Module 1</h4>
+                        <h3>NFT Basics</h3>
+                    </div>
+                    <div className={styles.module_img}>
+                        <img src={nft} alt="nft name" />
+                    </div>
+                </article>
+
+                <article className={styles.module}>
+                    <div className={styles.module_text}>
+                        <h4>Module 2</h4>
+                        <h3>Registration for the required services</h3>
+                    </div>
+                    <div className={styles.module_img}>
+                        <img src={nft} alt="nft name" />
+                    </div>
+                </article>
+
+                <article className={styles.module}>
+                    <div className={styles.module_text}>
+                        <h4>Module 3</h4>
+                        <h3>Your First NFT Trade</h3>
+                    </div>
+                    <div className={styles.module_img}>
+                        <img src={nft} alt="nft name" />
+                    </div>
+                </article>
+
+                <article className={styles.module}>
+                    <div className={styles.module_text}>
+                        <h4>Module 4</h4>
+                        <h3>How and where to find a quality project</h3>
+                    </div>
+                    <div className={styles.module_img}>
+                        <img src={nft} alt="nft name" />
+                    </div>
+                </article>
+
+                <article className={styles.module}>
+                    <div className={styles.module_text}>
+                        <h4>Module 5</h4>
+                        <h3>Scam in NFT</h3>
+                    </div>
+                    <div className={styles.module_img}>
+                        <img src={nft} alt="nft name" />
+                    </div>
+                </article>
+
+                <article className={styles.module}>
+                    <div className={styles.module_text}>
+                        <h4>Module 6</h4>
+                        <h3>Registration for the required services</h3>
+                    </div>
+                    <div className={styles.module_img}>
+                        <img src={nft} alt="nft name" />
+                    </div>
+                </article>
+
+                <article className={styles.module}>
+                    <div className={`${styles.module_text} ${styles.module_text_no_image}`}>
+                        <h4>Module 7</h4>
+                        <h3>Influencers</h3>
+                        <p>In our final module, we will provide you with links to 
+                            all useful influencers in the sphere. As well as YouTube
+                             channels, discord groups, both paid and free. We recommend 
+                             you to install Twitter application on your smartphone, 
+                             and turn on notifications from all Influencers on the list, 
+                             in order not to miss anything, as well as to watch for updates 
+                             on YouTube channels and get new information, and of course do not 
+                             forget about the discord, because there you can find new friends, 
+                             which will help you in the future!</p>
+                    </div>
+                </article>
+
+            </div>
+
+        </section>
+    );
+};
+
+export default Content;
